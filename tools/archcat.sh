f18761156bac3c570a36b4416e23a0abc6bff3e6
ThomasMTT <thomasmatthys@protonmail.com>
2024-07-26 02:49:01 +0200

    Upload tools

---
+++ tools/archcat.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#       _             _      ____      _      ____ _     ___
+#      / \   _ __ ___| |__  / ___|__ _| |_   / ___| |   |_ _|
+#     / _ \ | '__/ __| '_ \| |   / _` | __| | |   | |    | |
+#    / ___ \| | | (__| | | | |__| (_| | |_  | |___| |___ | |
+#   /_/   \_\_|  \___|_| |_|\____\__,_|\__|  \____|_____|___|
+#
+# Downloads, installs and runs the archcat cli tool from https://github.com/ThomasMTT/Archcat-cli
+
+# Define ANSI escape codes for color
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+        echo -e " [$RED*$RESET] This script must be run as root. Use 'sudo archcat'"
+        exit 1
+fi
+
+FILE_URL="https://github.com/ThomasMTT/Archcat-cli/raw/main/archcat.sh"
+DESTINATION="/usr/local/bin/archcat"
+TMP_FILE="/tmp/archcat-cli-tmp"
+
+# Download the file
+echo -e " [$YELLOW*$RESET] Downloading latest version of archcat..."
+curl -sSL -o "$TMP_FILE" "$FILE_URL"
+
+# Check if the download was successful
+if [[ $? -ne 0 ]]; then
+        echo -e " [$RED*$RESET] Error downloading the archcat"
+        exit 1
+fi
+
+# Move the downloaded file to /usr/local/bin
+echo -e " [$YELLOW*$RESET] Moving file to $DESTINATION..."
+mv "$TMP_FILE" "$DESTINATION"
+
+# Check if the move was successful
+if [[ $? -ne 0 ]]; then
+        rm "$TMP_FILE" 2>/dev/null
+        echo -e " [$RED*$RESET] Error moving the file to /usr/local/bin. Please check permissions."
+        exit 1
+fi
+
+# Ensure the file is executable
+chmod +x "$DESTINATION"
+
+echo -e " [$GREEN*$RESET] Archcat Succesfully installed.\n [$GREEN*$RESET] Running Archcat"
+
+# Run the command
+/usr/local/bin/archcat
---
+++ tools/archgen.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+#     ___                    _        ___
+#    /   \     _ _    __    | |_     / __|    ___    _ _
+#    | - |    | '_|  / _|   | ' \   | (_ |   / -_)  | ' \
+#    |_|_|   _|_|_   \__|_  |_||_|   \___|   \___|  |_||_|
+#  _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|
+#  "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
+
+# Helper function to handle script cancellation
+canceled() {
+        echo "Canceled. Exiting..."
+        exit 1
+}
+
+# Function to get a confirmed password
+get_confirmed_password() {
+        local user="$1"
+        while true; do
+                password=$(whiptail --title "Set Password" --passwordbox "Enter password for $user:" 8 40 3>&1 1>&2 2>&3)
+                if [[ $? -ne 0 ]]; then canceled; fi
+
+                confirm_password=$(whiptail --title "Confirm Password" --passwordbox "Confirm password for $user:" 8 40 3>&1 1>&2 2>&3)
+                if [[ $? -ne 0 ]]; then canceled; fi
+
+                if [ -z "$password" ] || [ -z "$confirm_password" ]; then
+                        whiptail --title "Empty Password" --msgbox "Password cannot be empty. Please enter a valid password." 8 60 3>&1 1>&2 2>&3
+                        if [[ $? -ne 0 ]]; then canceled; fi
+                elif [ "$password" == "$confirm_password" ]; then
+                        whiptail --title "Confirmed" --msgbox "Password confirmed." 8 40 3>&1 1>&2 2>&3
+                        if [[ $? -ne 0 ]]; then canceled; fi
+                        echo "$password"
+                        return
+                else
+                        whiptail --title "Mismatch" --msgbox "Passwords do not match. Please try again." 8 60 3>&1 1>&2 2>&3
+                        if [[ $? -ne 0 ]]; then canceled; fi
+                fi
+        done
+}
+
+# Detect virtual machine environments
+virtual_machine=""
+dmidecode | grep -qi "VirtualBox" && virtual_machine="VirtualBox"
+dmidecode | grep -qi "VMware" && virtual_machine="VMware"
+
+# Choose partition scheme (with only DOS supported for now)
+partition_scheme="msdos"
+
+# Get list of disks
+disks=($(lsblk -b -o NAME,SIZE,TYPE | awk '$3 == "disk" {print $1, int($2 / (1024 ** 3))"GB"}'))
+
+# Create menu options for disk selection
+available_drives=()
+for ((i = 0; i < ${#disks[@]}; i += 2)); do
+        available_drives+=("${disks[$i]}" "Size: ${disks[$i + 1]}")
+done
+
+# Select drive for installation
+selected_drive=$(whiptail --title "Select a Disk" --menu "WARNING: The selected disk will be formatted." 15 50 4 "${available_drives[@]}" 3>&1 1>&2 2>&3)
+if [[ $? -ne 0 ]]; then canceled; fi
+
+# Get root password
+root_password=$(get_confirmed_password "root")
+if [[ $? -ne 0 ]]; then canceled; fi
+
+# Get username
+while true; do
+        username=$(whiptail --title "Username" --inputbox "Enter a username:" 8 40 3>&1 1>&2 2>&3)
+        if [[ $? -ne 0 ]]; then canceled; fi
+        if [ -z "$username" ]; then
+                whiptail --title "Invalid Username" --msgbox "Username cannot be empty. Please enter a valid username." 8 60 3>&1 1>&2 2>&3
+                if [[ $? -ne 0 ]]; then canceled; fi
+        else
+                break
+        fi
+done
+
+# Get user password
+user_password=$(get_confirmed_password "$username")
+if [[ $? -ne 0 ]]; then canceled; fi
+
+# Get hostname
+hostname=$(whiptail --title "Hostname" --inputbox "Enter a hostname:" 8 40 3>&1 1>&2 2>&3)
+if [[ $? -ne 0 ]]; then canceled; fi
+
+# Check if virtual machine extensions should be installed
+virtual_machine_ext="false"
+if [ -n "$virtual_machine" ]; then
+        whiptail --title "${virtual_machine} Extensions" --yesno "Install ${virtual_machine} extensions for seamless screen and shared clipboard?" 8 45 3>&1 1>&2 2>&3
+        if [[ $? -ne 0 ]]; then canceled; fi
+        if [[ $? -eq 0 ]]; then
+                virtual_machine_ext="true"
+        fi
+fi
+
+# Configuration file creation
+config_file="archgen.cfg"
+
+# Confirm values or exit
+clear
+echo """
+PARTITION_SCHEME: $partition_scheme
+SELECTED_DRIVE: $selected_drive
+ROOT_PASSWORD: [HIDDEN]
+USERNAME: $username
+USER_PASSWORD: [HIDDEN]
+HOSTNAME: $hostname
+VIRTUAL_MACHINE: $virtual_machine
+VIRTUAL_MACHINE_EXT: $virtual_machine_ext
+
+ENTER TO CONFIRM THESE VALUES AND WRITE CONFIGURATION FILE
+CTRL-C TO CANCEL FILE GENERATION
+"""
+read -r
+
+# Write ASCII art to configuration file
+cat >"$config_file" <<'EOL'
+#!/bin/bash
+#     ___                    _        ___
+#    /   \     _ _    __    | |_     / __|    ___    _ _
+#    | - |    | '_|  / _|   | ' \   | (_ |   / -_)  | ' \
+#    |_|_|   _|_|_   \__|_  |_||_|   \___|   \___|  |_||_|
+#  _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|
+#  "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
+
+EOL
+
+# Append configuration details to the file
+cat >>"$config_file" <<EOL
+PARTITION_SCHEME="$partition_scheme"
+SELECTED_DRIVE="$selected_drive"
+ROOT_PASSWORD="$root_password"
+USERNAME="$username"
+USER_PASSWORD="$user_password"
+HOSTNAME="$hostname"
+VIRTUAL_MACHINE="$virtual_machine"
+VIRTUAL_MACHINE_EXT="$virtual_machine_ext"
+EOL
+
+# Cleanup trailing whitespace
+sed -i 's/[[:space:]]*$//' "$config_file"
+
+# Notify the user of successful completion
+whiptail --title "ArchGen" --msgbox "Configuration file '$config_file' created successfully." 8 40 3>&1 1>&2 2>&3
+
+# Clear the screen
+clear
